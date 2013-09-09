f9109c06876e4ebbd327788111f3e0674ad5a153
Pete Otaqui <pete@otaqui.com>
2013-09-09 04:38:56 -0400

    added 20 minute auto-refresh to feeds and page

--- newslick.js
+++ newslick.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var Q = require('q');
 
+
+var refresh_rate = 20 * 60 * 1000; // 20 minutes;
+
+
 var stories = [
     {
         "title": "Loading",
@@ -27,19 +31,20 @@ app.use(express.static('public'));
 
 
 app.get('/', function(req, res) {
-    res.render('index.ejs', {"stories": stories});
+    res.render('index.ejs', {"stories": stories, "refresh_rate": refresh_rate});
 });
 
-loadFeeds()
-    .then(function(data) {
-        var s = [];
-        data.forEach(function(feed, index) {
-            if ( index > 3 ) return;
-            s = s.concat(feed.stories);
+setInterval(function() {
+    loadFeeds()
+        .then(function(data) {
+            var s = [];
+            data.forEach(function(feed, index) {
+                if ( index > 3 ) return;
+                s = s.concat(feed.stories);
+            });
+            stories = s;
         });
-        console.log('s', s.length);
-        stories = s;
-    });
+}, 1000 * 60 * 20);
 
 var port = process.env.PORT || 5000;
 app.listen(port);
